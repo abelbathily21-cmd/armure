98c2cacb5f21a2b39f1c659970c6f7aa1838a90f
abelbathily21-cmd <abel.bathily21@gmail.com>
2026-09-01 02:23:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "armure-v1";
+const CACHE = "armure-v2";
 const SHELL = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))); self.clients.claim(); });
